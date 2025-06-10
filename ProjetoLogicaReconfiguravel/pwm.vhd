85f12578911bfc6c94bb6f70259e90c73c5795d0
Yan Danilo Lima Morejon <yan.dl.morejon@gmail.com>
2025-06-10 08:09:15 -0300

    update

--- ProjetoLogicaReconfiguravel/pwm.vhd
+++ ProjetoLogicaReconfiguravel/pwm.vhd
@@ -3,6 +3,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
+-- pwm de 4 bits
 
 entity pwm is
 
@@ -23,8 +24,38 @@ architecture main of pwm is
             signal count : std_logic_vector(3 downto 0);
 
 begin
-    
+
             
+
+
+            cnt : process(clk, clr) 
+            begin
+
+                    if clr = '1' then 
+
+                        count <= "0000";
+                    elsif clk'event and clk = '1' then        
+                            if count = period - 1 then
+                                count <= "0000";
+                            else 
+                                count <= count + 1;
+                            end if;    
+
+                    end if;    
+
+            end process;        
+            
+
+            pwmout : process(count)
+            
+            begin
+                    if count < duty then 
+                        pwm <= '1';
+                    else 
+                        pwm <= '0';
+                    end if;        
+
+            end process;
 
 
 
